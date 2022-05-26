fa6efc1772e44d8e350732169e5fa6e1be72e83b
Nicolas <nicogamarra2010@gmail.com>
2022-05-26 12:07:58 -0300

    cambios en la estetica de redesScociales

--- src/app/componentes/redes/redes.component.ts
+++ src/app/componentes/redes/redes.component.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { DatosPorfolioService } from 'src/app/servicios/datos-porfolio.service';
 import { RedesSociales } from 'src/app/servicios/interfaces/RedesSociales';
@@ -13,8 +14,9 @@ export class RedesComponent implements OnInit {
   login:any;
   datosPorfolio!: RedesSociales [];
   componente:string = "redesSociales";
+  pantallasPequenias:boolean = false;
 
-  constructor(private loginService: LoginServiceService, private datosDb:DatosPorfolioService) { }
+  constructor(private loginService: LoginServiceService, private datosDb:DatosPorfolioService, private responsive: BreakpointObserver) { }
 
   ngOnInit(): void {
     /* this.login = this.loginService.getLogueado() */
@@ -29,6 +31,16 @@ export class RedesComponent implements OnInit {
       this.datosPorfolio = datos});
     
   }
+  vistaResponsiva(){
+  this.responsive.observe('(max-width: 768px)')
+      .subscribe(result => {
 
+        this.pantallasPequenias = false; 
 
+        if (result.matches) {
+          this.pantallasPequenias = true;
+        }
+
+  });
+  }
 }
